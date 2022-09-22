b4deeec8453bc720c7658146a7acb9544e4b4dd8
Kaizen Conroy <zen.conroy@gmail.com>
2022-09-22 15:56:03 -0400

    feat: add integ test

---
+++ test/node-proxy-agent-asset.integ.ts
@@ -0,0 +1,35 @@
+import * as path from 'path';
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as cr from 'aws-cdk-lib/custom-resources';
+
+import { NodeProxyAgentAsset } from '../lib';
+
+/**
+ * Test verifies that AWS CLI is invoked successfully inside Lambda runtime.
+ */
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'lambda-layer-node-proxy-agent-integ-stack');
+const asset = new NodeProxyAgentAsset(stack, 'node-proxy-asset');
+const layer = new lambda.LayerVersion(stack, 'NodeProxyLayer', {
+  code: lambda.Code.fromBucket(asset.bucket, asset.s3ObjectKey),
+  description: '/opt/nodejs/node_modules/proxy-agent',
+});
+
+const provider = new cr.Provider(stack, 'ProviderNode14', {
+  onEventHandler: new lambda.Function(stack, 'Lambda$Node14', {
+    code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')),
+    handler: 'index.handler',
+    runtime: lambda.Runtime.NODEJS_14_X,
+    layers: [layer],
+    memorySize: 512,
+    timeout: cdk.Duration.seconds(30),
+  }),
+});
+
+new cdk.CustomResource(stack, 'CustomResourceNode14', {
+  serviceToken: provider.serviceToken,
+});
+
+app.synth();
