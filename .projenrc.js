f2f04f282bcf9c312260d90cdbefc97e4f12787d
Kaizen Conroy <zen.conroy@gmail.com>
2022-09-22 16:03:13 -0400

    feat: release node proxy agent asset v5

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,9 @@
 const { awscdk, JsonPatch } = require('projen');
+const { NpmAccess } = require('projen/lib/javascript');
 
+// the version of proxy-agent that this branch supports
 const SPEC_VERSION = '5';
+const releaseWorkflowName = `release-node-proxy-agent-v${SPEC_VERSION}`;
 
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Amazon Web Services',
@@ -21,13 +24,38 @@ const project = new awscdk.AwsCdkConstructLibrary({
       run: 'sudo chown superchain /var/run/docker.sock',
     },
   ],
+  npmAccess: NpmAccess.PUBLIC,
+  releaseTagPrefix: `node-proxy-agent-v${SPEC_VERSION}`,
+  releaseWorkflowName: releaseWorkflowName,
+  defaultReleaseBranch: 'main',
+  publishToPypi: {
+    distName: `aws-cdk.asset-node-proxy-agent-v${SPEC_VERSION}`,
+    module: `aws_cdk.asset_node_proxy_agent_v${SPEC_VERSION}`,
+  },
+  publishToMaven: {
+    javaPackage: `software.amazon.awscdk.cdk.asset.node.proxy.agent.v${SPEC_VERSION}`,
+    mavenGroupId: 'software.amazon.awscdk',
+    mavenArtifactId: `cdk-asset-node-proxy-agent-v${SPEC_VERSION}`,
+    mavenEndpoint: 'https://aws.oss.sonatype.org',
+  },
+  publishToNuget: {
+    dotNetNamespace: `Amazon.CDK.Asset.NodeProxyAgentV${SPEC_VERSION}`,
+    packageId: `Amazon.CDK.Asset.NodeProxyAgentV${SPEC_VERSION}`,
+  },
+  publishToGo: {
+    moduleName: 'github.com/cdklabs/awscdk-asset-node-proxy-agent-go',
+    packageName: `nodeproxyagentv${SPEC_VERSION}`,
+    gitUserName: 'AWS CDK Team',
+    gitUserEmail: 'aws-cdk@amazon.com',
+    githubTokenSecret: 'PROJEN_GITHUB_TOKEN',
+  },
 });
 
 // These patches are required to enable sudo commands in the workflows under `workflowBootstrapSteps`,
 // see `workflowBootstrapSteps` above for why a sudo command is needed.
 const buildWorkflow = project.tryFindObjectFile('.github/workflows/build.yml');
 buildWorkflow.patch(JsonPatch.add('/jobs/build/container/options', '--group-add sudo'));
-const releaseWorkflow = project.tryFindObjectFile('.github/workflows/release.yml');
+const releaseWorkflow = project.tryFindObjectFile(`.github/workflows/${releaseWorkflowName}.yml`);
 releaseWorkflow.patch(JsonPatch.add('/jobs/release/container/options', '--group-add sudo'));
 const upgradeWorkflow = project.tryFindObjectFile('.github/workflows/upgrade-main.yml');
 upgradeWorkflow.patch(JsonPatch.add('/jobs/upgrade/container/options', '--group-add sudo'));
