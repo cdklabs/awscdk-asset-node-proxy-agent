4cad554b5a3e570defe6b7735a5d04d1496179b4
Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>
2022-10-14 14:10:47 -0400

    feat: initial node proxy agent layer construct (#1)
    * feat: initial node proxy agent layer construct
    
    * remove unnecessarily copied files

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,37 @@
-const { awscdk } = require('projen');
-const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Kaizen Conroy',
-  authorAddress: 'zen.conroy@gmail.com',
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  name: 'awscdk-asset-node-proxy-agent',
-  repositoryUrl: 'https://github.com/cdklabs/awscdk-asset-node-proxy-agent.git',
+const { awscdk, JsonPatch } = require('projen');
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+const SPEC_VERSION = '5';
+
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Amazon Web Services',
+  cdkVersion: '2.0.0',
+  defaultReleaseBranch: 'main',
+  name: `@awscdk/asset-node-proxy-agent-v${SPEC_VERSION}`,
+  repositoryUrl: 'https://github.com/cdklabs/awscdk-asset-node-proxy-agent.git',
+  homepage: 'https://github.com/cdklabs/awscdk-asset-node-proxy-agent#readme',
+  autoApproveOptions: {
+    allowedUsernames: ['cdklabs-automation'],
+    secret: 'GITHUB_TOKEN',
+  },
+  autoApproveUpgrades: true,
+  workflowBootstrapSteps: [
+    {
+      // This step is required to allow the build workflow to build docker images
+      name: 'Change permissions on /var/run/docker.sock',
+      run: 'sudo chown superchain /var/run/docker.sock',
+    },
+  ],
 });
+
+// These patches are required to enable sudo commands in the workflows under `workflowBootstrapSteps`,
+// see `workflowBootstrapSteps` above for why a sudo command is needed.
+const buildWorkflow = project.tryFindObjectFile('.github/workflows/build.yml');
+buildWorkflow.patch(JsonPatch.add('/jobs/build/container/options', '--group-add sudo'));
+const releaseWorkflow = project.tryFindObjectFile('.github/workflows/release.yml');
+releaseWorkflow.patch(JsonPatch.add('/jobs/release/container/options', '--group-add sudo'));
+const upgradeWorkflow = project.tryFindObjectFile('.github/workflows/upgrade-main.yml');
+upgradeWorkflow.patch(JsonPatch.add('/jobs/upgrade/container/options', '--group-add sudo'));
+
+project.preCompileTask.exec('layer/build.sh');
+
 project.synth();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
-  }
-}
+export * from './node-proxy-agent-layer';
