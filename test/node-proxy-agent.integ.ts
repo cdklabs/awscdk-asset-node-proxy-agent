dc6958baa0660ac8d1d75095ab7a29921dc70094
Kaizen Conroy <zen.conroy@gmail.com>
2022-10-31 17:27:16 -0400

    feat: expose `LAYER_SOURCE and ASSET_FILE consts

---
+++ test/node-proxy-agent.integ.ts
@@ -0,0 +1,40 @@
+import * as path from 'path';
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3_assets from 'aws-cdk-lib/aws-s3-assets';
+import * as cr from 'aws-cdk-lib/custom-resources';
+
+import { ASSET_FILE, LAYER_SOURCE } from '../lib';
+import { hashFile } from './util';
+
+/**
+ * Test verifies that AWS CLI is invoked successfully inside Lambda runtime.
+ */
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, 'lambda-layer-node-proxy-agent-integ-stack');
+const asset = new s3_assets.Asset(stack, 'node-proxy-asset', {
+  path: ASSET_FILE,
+  assetHash: hashFile(LAYER_SOURCE),
+});
+const layer = new lambda.LayerVersion(stack, 'NodeProxyLayer', {
+  code: lambda.Code.fromBucket(asset.bucket, asset.s3ObjectKey),
+  description: '/opt/nodejs/node_modules/proxy-agent',
+});
+
+const provider = new cr.Provider(stack, 'ProviderNode14', {
+  onEventHandler: new lambda.Function(stack, 'Lambda$Node14', {
+    code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')),
+    handler: 'index.handler',
+    runtime: lambda.Runtime.NODEJS_14_X,
+    layers: [layer],
+    memorySize: 512,
+    timeout: cdk.Duration.seconds(30),
+  }),
+});
+
+new cdk.CustomResource(stack, 'CustomResourceNode14', {
+  serviceToken: provider.serviceToken,
+});
+
+app.synth();
--- test/proxy-agent-layer.test.ts
+++ test/proxy-agent-layer.test.ts
@@ -1,13 +1,20 @@
 import { Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import { NodeProxyAgentLayer } from '../lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { ASSET_FILE, LAYER_SOURCE } from '../lib';
+import { hashFile } from './util';
 
 test('synthesized to a layer version', () => {
   //GIVEN
   const stack = new Stack();
 
   // WHEN
-  new NodeProxyAgentLayer(stack, 'MyLayer');
+  new lambda.LayerVersion(stack, 'MyLayer', {
+    code: lambda.Code.fromAsset(ASSET_FILE, {
+      assetHash: hashFile(LAYER_SOURCE),
+    }),
+    description: '/opt/nodejs/node_modules/proxy-agent',
+  });
 
   // THEN
   Template.fromStack(stack).hasResourceProperties('AWS::Lambda::LayerVersion', {
