a35eb05238dbacbe499316fca03b9e757fc6641b
Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>
2022-11-02 18:28:55 -0400

    feat: expose `LAYER_SOURCE`, `LAYER_SOURCE_DIR`, and `ASSET_FILE` consts (#14)
    * feat: expose `LAYER_SOURCE and ASSET_FILE consts
    
    * readme
    
    * Update test/node-proxy-agent.integ.ts
    
    * feat: change `LAYER_SOURCE` to `LAYER_SOURCE_DIR` and move major version to 2 (#15)
    
    Co-authored-by: Madeline Kusters <80541297+madeline-k@users.noreply.github.com>

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,9 @@
-const { awscdk, JsonPatch } = require('projen');
+const { awscdk, JsonPatch, DependencyType } = require('projen');
+const { NpmAccess } = require('projen/lib/javascript');
 
+// the version of proxy-agent that this branch supports
 const SPEC_VERSION = '5';
+const releaseWorkflowName = `release-node-proxy-agent-v${SPEC_VERSION}`;
 
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Amazon Web Services',
@@ -10,7 +13,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   repositoryUrl: 'https://github.com/cdklabs/awscdk-asset-node-proxy-agent.git',
   homepage: 'https://github.com/cdklabs/awscdk-asset-node-proxy-agent#readme',
   autoApproveOptions: {
-    allowedUsernames: ['cdklabs-automation'],
+    allowedUsernames: ['aws-cdk-automation'],
     secret: 'GITHUB_TOKEN',
   },
   autoApproveUpgrades: true,
@@ -21,13 +24,46 @@ const project = new awscdk.AwsCdkConstructLibrary({
       run: 'sudo chown superchain /var/run/docker.sock',
     },
   ],
+  majorVersion: 2,
+  npmAccess: NpmAccess.PUBLIC,
+  releaseTagPrefix: `node-proxy-agent-v${SPEC_VERSION}`,
+  releaseWorkflowName: releaseWorkflowName,
+  defaultReleaseBranch: 'main',
+  publishToPypi: {
+    distName: `aws-cdk.asset-node-proxy-agent-v${SPEC_VERSION}`,
+    module: `aws_cdk.asset_node_proxy_agent_v${SPEC_VERSION}`,
+  },
+  publishToMaven: {
+    javaPackage: `software.amazon.awscdk.cdk.asset.node.proxy.agent.v${SPEC_VERSION}`,
+    mavenGroupId: 'software.amazon.awscdk',
+    mavenArtifactId: `cdk-asset-node-proxy-agent-v${SPEC_VERSION}`,
+    mavenEndpoint: 'https://aws.oss.sonatype.org',
+  },
+  publishToNuget: {
+    dotNetNamespace: `Amazon.CDK.Asset.NodeProxyAgentV${SPEC_VERSION}`,
+    packageId: `Amazon.CDK.Asset.NodeProxyAgentV${SPEC_VERSION}`,
+  },
+  publishToGo: {
+    moduleName: 'github.com/cdklabs/awscdk-asset-node-proxy-agent-go',
+    packageName: `nodeproxyagentv${SPEC_VERSION}`,
+    gitUserName: 'AWS CDK Team',
+    gitUserEmail: 'aws-cdk@amazon.com',
+    githubTokenSecret: 'PROJEN_GITHUB_TOKEN',
+  },
 });
+
+// We only need aws-cdk-lib and constructs for testing. Neither library is used
+// in the public API.
+project.deps.removeDependency('constructs', DependencyType.PEER);
+project.deps.addDependency('constructs@^10.0.5', DependencyType.DEVENV);
+project.deps.removeDependency('aws-cdk-lib', DependencyType.PEER);
+project.deps.addDependency('aws-cdk-lib@^2.0.0', DependencyType.DEVENV);
 
 // These patches are required to enable sudo commands in the workflows under `workflowBootstrapSteps`,
 // see `workflowBootstrapSteps` above for why a sudo command is needed.
 const buildWorkflow = project.tryFindObjectFile('.github/workflows/build.yml');
 buildWorkflow.patch(JsonPatch.add('/jobs/build/container/options', '--group-add sudo'));
-const releaseWorkflow = project.tryFindObjectFile('.github/workflows/release.yml');
+const releaseWorkflow = project.tryFindObjectFile(`.github/workflows/${releaseWorkflowName}.yml`);
 releaseWorkflow.patch(JsonPatch.add('/jobs/release/container/options', '--group-add sudo'));
 const upgradeWorkflow = project.tryFindObjectFile('.github/workflows/upgrade-main.yml');
 upgradeWorkflow.patch(JsonPatch.add('/jobs/upgrade/container/options', '--group-add sudo'));
--- test/proxy-agent-layer.test.ts
+++ test/proxy-agent-layer.test.ts
@@ -1,13 +1,19 @@
-import { Stack } from 'aws-cdk-lib';
+import { FileSystem, Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import { NodeProxyAgentLayer } from '../lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { ASSET_FILE, LAYER_SOURCE_DIR } from '../lib';
 
 test('synthesized to a layer version', () => {
   //GIVEN
   const stack = new Stack();
 
   // WHEN
-  new NodeProxyAgentLayer(stack, 'MyLayer');
+  new lambda.LayerVersion(stack, 'MyLayer', {
+    code: lambda.Code.fromAsset(ASSET_FILE, {
+      assetHash: FileSystem.fingerprint(LAYER_SOURCE_DIR),
+    }),
+    description: '/opt/nodejs/node_modules/proxy-agent',
+  });
 
   // THEN
   Template.fromStack(stack).hasResourceProperties('AWS::Lambda::LayerVersion', {
