3bd93e56bebbeabee8a3c9335d33ac5068b8cfff
AWS CDK Automation <43080478+aws-cdk-automation@users.noreply.github.com>
2023-01-24 14:34:04 -0800

    chore(deps): upgrade dependencies (#54)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/cdklabs/awscdk-asset-node-proxy-agent/actions/runs/3991711862
    
    ------
    
    *Automatically created by projen via the "upgrade-main" workflow*

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,15 @@
-const { awscdk, JsonPatch, DependencyType } = require('projen');
-const { NpmAccess } = require('projen/lib/javascript');
+import { awscdk, DependencyType } from 'projen';
+import { NpmAccess } from 'projen/lib/javascript';
+import { WorkflowNoDockerPatch } from './projenrc/workflow-no-docker-patch';
 
 // the version of proxy-agent that this branch supports
 const SPEC_VERSION = '5';
 const releaseWorkflowName = `release-node-proxy-agent-v${SPEC_VERSION}`;
 
 const project = new awscdk.AwsCdkConstructLibrary({
+  projenrcTs: true,
   author: 'Amazon Web Services',
+  authorAddress: 'aws-cdk-dev@amazon.com',
   cdkVersion: '2.0.0',
   defaultReleaseBranch: 'main',
   name: `@aws-cdk/asset-node-proxy-agent-v${SPEC_VERSION}`,
@@ -17,18 +20,10 @@ const project = new awscdk.AwsCdkConstructLibrary({
     secret: 'GITHUB_TOKEN',
   },
   autoApproveUpgrades: true,
-  workflowBootstrapSteps: [
-    {
-      // This step is required to allow the build workflow to build docker images
-      name: 'Change permissions on /var/run/docker.sock',
-      run: 'sudo chown superchain /var/run/docker.sock',
-    },
-  ],
   majorVersion: 2,
   npmAccess: NpmAccess.PUBLIC,
   releaseTagPrefix: `node-proxy-agent-v${SPEC_VERSION}`,
   releaseWorkflowName: releaseWorkflowName,
-  defaultReleaseBranch: 'main',
   publishToPypi: {
     distName: `aws-cdk.asset-node-proxy-agent-v${SPEC_VERSION}`,
     module: `aws_cdk.asset_node_proxy_agent_v${SPEC_VERSION}`,
@@ -59,14 +54,9 @@ project.deps.addDependency('constructs@^10.0.5', DependencyType.DEVENV);
 project.deps.removeDependency('aws-cdk-lib', DependencyType.PEER);
 project.deps.addDependency('aws-cdk-lib@^2.0.0', DependencyType.DEVENV);
 
-// These patches are required to enable sudo commands in the workflows under `workflowBootstrapSteps`,
-// see `workflowBootstrapSteps` above for why a sudo command is needed.
-const buildWorkflow = project.tryFindObjectFile('.github/workflows/build.yml');
-buildWorkflow.patch(JsonPatch.add('/jobs/build/container/options', '--group-add sudo'));
-const releaseWorkflow = project.tryFindObjectFile(`.github/workflows/${releaseWorkflowName}.yml`);
-releaseWorkflow.patch(JsonPatch.add('/jobs/release/container/options', '--group-add sudo'));
-const upgradeWorkflow = project.tryFindObjectFile('.github/workflows/upgrade-main.yml');
-upgradeWorkflow.patch(JsonPatch.add('/jobs/upgrade/container/options', '--group-add sudo'));
+// Fix Docker on GitHub
+new WorkflowNoDockerPatch(project, { workflow: 'build' });
+new WorkflowNoDockerPatch(project, { workflow: 'release' });
 
 project.preCompileTask.exec('layer/build.sh');
 
